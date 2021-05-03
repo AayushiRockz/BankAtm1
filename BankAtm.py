2ffe229603bca290a7006ff2c539d7fd1115b9ea
AayushiRockz <71376337+AayushiRockz@users.noreply.github.com>
2021-05-03 23:50:08 +0530

    Add files via upload
    Methods and their print statements

---
+++ BankAtm.py
@@ -0,0 +1,16 @@
+class atm(object):
+    def __init__(self,atmNumber,pinNumber):
+        self.atmNumber = atmNumber
+        self.pinNumber = pinNumber
+
+    def CashWithDrawal(self):
+        print("Cash withdrawed")
+
+    def BalanceEnquiry(self):
+        print("Balance in account")
+
+    def CashDeposit(self):
+        print("Cash Deposited")
+
+        
+
